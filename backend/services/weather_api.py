f485cca2f49b558acf4cdb67b9cd51d0af6491f2
Jeothish <jeothishsenthil@gmail.com>
2025-10-14 23:44:33 +0100

    Current weather formatted to only show relevant information

--- backend/services/weather_api.py
+++ backend/services/weather_api.py
@@ -3,7 +3,7 @@ weather_api.py
 
 Description:
     This script connects to the OpenWeatherMap API to get current weather and 5 day forecast data for a given city.
-    The data from the API is then formatted further
+    The data from the API is then formatted further using a helper function.
 
 Author:
     Jeothish Senthilkumar
@@ -13,7 +13,7 @@ Author:
 
 
 import requests
-from datetime import datetime
+from datetime import datetime,timezone,timedelta
 
 API_KEY = "f57e4e15a233f51887069167cb3b8bd4"
 UNITS = "metric"
@@ -47,12 +47,38 @@ def get_forecast(city_name):
     return response.json()
 
 #TO DO
-def format_weather_data(city_name):
-    current_weather_data = get_current_weather(city_name)
-    forecast_data = get_forecast(city_name)
+def format_current_weather_data(current_weather_data):
+    city = current_weather_data["name"]
+    country = current_weather_data["sys"]["country"]
+
+    timezone_offset = current_weather_data["timezone"]
+    timestamp = current_weather_data["dt"]
+    local_time = datetime.fromtimestamp(timestamp,timezone.utc) + timedelta(seconds=timezone_offset)
+    formatted_time = local_time.strftime("%d %B %I:%M %p")
+    
+
+    current_weather = {
+        "Temperature" : current_weather_data["main"]["temp"],
+        "Feels-Like" : current_weather_data["main"]["feels_like"],
+        "Weather Condition" : current_weather_data["weather"][0]["description"],
+        "Humidity" : current_weather_data["main"]["humidity"],
+        "Wind-Speed" : current_weather_data["wind"]["speed"],
+        "Local Time" : formatted_time
+     }
+    
+    return{
+        "City": city,
+        "Country" : country,
+        "Current Weather" : current_weather
+    }
+
+    
 
 if __name__ == "__main__":
     city_name = input("Enter the city name: ")
-    print(f"======CURRENT WEATHER IN {city_name}==== \n" , get_current_weather(city_name))
-    print()
-    print(f"=====FORECAST IN {city_name}===== \n" , get_forecast(city_name))
+    #print(f"======CURRENT WEATHER IN {city_name}==== \n" , get_current_weather(city_name))
+    #print()
+    #print(f"=====FORECAST IN {city_name}===== \n" , get_forecast(city_name))
+    current_weather_data = get_current_weather(city_name)
+    print(format_current_weather_data(current_weather_data))
+
