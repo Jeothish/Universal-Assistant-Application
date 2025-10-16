0380fc9a8ad7ee877e2e624d661f1607d4101b90
Jeothish <jeothishsenthil@gmail.com>
2025-10-16 16:49:17 +0100

    Initial start to news api

--- backend/services/news_api..py
+++ backend/services/news_api..py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime,timezone,timedelta
 
+
 API_KEY = "pub_a5b78b04509c4f26a27113fd9ae03147"
 
 def get_news(city,topic=None,date=datetime.now(),language="en",sort_by = "publishedAt"):
