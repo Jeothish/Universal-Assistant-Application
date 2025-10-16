2c975a85b210387d9e35737b53f2e6567e7b6ec7
Jeothish <jeothishsenthil@gmail.com>
2025-10-16 18:50:35 +0100

    Endpoints for news api and horuly forecasts

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 """
 app.py
 
-Flask server providing endpoints for current and forecasted weather data
+Flask server providing endpoints for: 
+                    current and forecasted weather data
+                    news data
 
 Author:
     Jeothish Senthilkumar
@@ -12,7 +14,9 @@ Author:
 
 from flask import Flask,request,jsonify
 from datetime import datetime,timedelta
-from services.weather_api import get_current_weather,get_forecast_weather,get_coordinates
+from services.weather_api import get_current_weather,get_forecast_weather_specific_time,get_coordinates,get_forecast_weather_day
+from services.news_api import get_news
+
 app = Flask(__name__)
 
 @app.route("/weather/current",methods=["GET"])
@@ -45,8 +49,8 @@ def current_weather():
                     "weather": data
                     })
 
-@app.route("/weather/forecast",methods=["GET"])
-def forecast_weather():
+@app.route("/weather/forecast/specific",methods=["GET"])
+def forecast_weather_specific():
     """
     API endpoint to fetch forecasted weather data for a future day and hour
 
@@ -75,16 +79,75 @@ def forecast_weather():
     target_datetime = datetime.now() + timedelta(days=days_ahead)
     target_datetime = target_datetime.replace(hour=hour, minute=0,second=0,microsecond=0)
 
-    forecast = get_forecast_weather(latitude,longitude,target_datetime)
+    forecast = get_forecast_weather_specific_time(latitude,longitude,target_datetime)
 
     return jsonify({
         "City": city,
         "Target_Time": target_datetime.strftime("%d %B %Y %H:%M"),
         "Forecast" : forecast
     })
+    
+@app.route("/weather/forecast/day",methods=["GET"])
+def forecast_weather_day():
+    """
+    API endpoint to fetch forecasted hourly weather data for a day  
 
+    Query Parameters:
+        city(str): Name of the city to forecast data
+        days(int): Number of days from today
 
+    Returns:
+        JSON : A dictionary containing forecasted hourly weather data for the given city for a given day  
+    """
+    city = request.args.get("city")
+    days_ahead = int(request.args.get("days",0))
+    
 
+    if not city:
+        return jsonify({"error": "No city selected"})
+    
+    coords = get_coordinates(city)
+
+    if not coords:
+        return jsonify({"error" : "City not found"})
+    
+    latitude,longitude = coords
+
+    target_day = datetime.now() + timedelta(days=days_ahead)
+    target_day = target_day.replace(minute=0,second=0,microsecond=0)
+
+    forecast = get_forecast_weather_day(latitude,longitude,target_day)
+
+    return jsonify({
+        "City": city,
+        "Target_Day": target_day.strftime("%d %B %Y %H:%M"),
+        "Forecast" : forecast
+    })    
+    
+@app.route("/news",methods=["GET"])    
+def news():
+    """
+    API endpoint to fetch news articles for a given city and topic
+
+    Query Parameters:
+        city (str): The name of the city you want news data for
+        topic (str, optional): The topic you want news data for. Defaults to None.
+        language (str, optional): Language of news. Defaults to "en".
+
+    Returns:
+        JSON : A dictionary containing news articles  for the given city and topic   
+    """
+    city = request.args.get("city")
+    topic = request.args.get("topic")
+    language = request.args.get("language")
+    
+    if not city:
+        return jsonify({"error": "No city selected"})
+    
+    news_data = get_news(city,topic,language)
+    return jsonify(news_data)
+    
+        
 
 if __name__ == "__main__":
     """
