41ecee7fcc95f185032e820b267ce3b49347eb85
Jeothish <jeothishsenthil@gmail.com>
2025-10-23 22:02:38 +0100

    Can add and delete reminders

--- backend/database/db.py
+++ backend/database/db.py
@@ -3,11 +3,11 @@ import os
 
 def get_connection():
     db_connection = psycopg2.connect(
-        dbname=os.environ.get("DB_NAME", "elderly_assistant_app"),
-        user=os.environ.get("DB_USER", ""),
-        password=os.environ.get("DB_PASS", ""),
-        host=os.environ.get("DB_HOST", "localhost"),
-        port=os.environ.get("DB_PORT", "5432"),
+    dbname="elderly_assistant_app",
+    user="",
+    password="",
+    host="localhost",
+    port="5432",
     )
     return db_connection
 
@@ -39,19 +39,50 @@ def add_city_db(city,latitude,longitude):
     connection.commit()
     cursor.close()
     connection.close()
-    
-def add_reminders_db(title,description,date):
+
+
+def get_reminders_db(rem_title=None,rem_date=None,rem_description=None):
      connection = get_connection()
      cursor = connection.cursor()
      
      QUERY = """
-     INSERT INTO reminders (title,reminder_description,reminder_date)
-     VALUES (%s,%s,%s)
+     SELECT (%s,%s,%s)
+     FROM reminders
+     WHERE
+     VALUES (%s,%s,%s,)
      """
-     cursor.execute(QUERY,(title,description,date))
+     cursor.execute(QUERY,(rem_title,rem_date,rem_description))
      connection.commit()
      cursor.close()
      connection.close()
+        
+def add_reminders_db(rem_title,rem_date,rem_description=None,is_complete=False,recur_type='none',recur_day_of_week=None,recur_time=None):
+     connection = get_connection()
+     cursor = connection.cursor()
+     
+     QUERY = """
+     INSERT INTO reminders (title,reminder_date,reminder_description,is_complete,recurrence_type,recurrence_day_of_week,recurrence_time)
+     VALUES (%s,%s,%s,%s,%s,%s,%s)
+     """
+     cursor.execute(QUERY,(rem_title,rem_date,rem_description,is_complete,recur_type,recur_day_of_week,recur_time))
+     connection.commit()
+     cursor.close()
+     connection.close()
+     
+def delete_reminders_db(reminder_id):
+    
+    connection = get_connection()
+    cursor = connection.cursor()
+     
+    QUERY = """
+    DELETE FROM reminders
+    WHERE reminder_id = %s
+    """
+    cursor.execute(QUERY,(reminder_id,))
+    connection.commit()
+    cursor.close()
+    connection.close()     
+     
      
     
      
