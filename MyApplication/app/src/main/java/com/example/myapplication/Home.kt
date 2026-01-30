492866d0d4ebc4ff6561290d2a646fcb45e3c75e
Jeothish <jeothishsenthil@gmail.com>
2026-01-30 17:16:51 +0000

    Screen switching implemented + Reminders Foundation

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Home.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun HomePage(modifier: Modifier = Modifier){
+
+
+    val navController = rememberNavController() //Keeps track of which screen is being displayed
+
+    //Connects screen tracker with the screens
+    NavHost(
+        navController = navController,
+        startDestination = HomeRoutes.MAIN,
+        modifier = modifier
+    ){
+        //Defines the home screen
+        composable(HomeRoutes.MAIN){
+            HomeMainScreen(
+                onOpenReminders = {
+                    navController.navigate(HomeRoutes.REMINDERS)
+                }
+            )
+        }
+        //Defines the reminders screen
+        composable(HomeRoutes.REMINDERS){
+            RemindersScreenDisplay(returnToChat = {navController.popBackStack()}) //Goes back to previous screen
+        }
+    }
+}
+
+
+
