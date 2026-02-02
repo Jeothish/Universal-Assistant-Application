08718dd967140472d66653f9ca1e56644aa07dc5
Jeothish <jeothishsenthil@gmail.com>
2026-02-02 17:55:43 +0000

    API calls to retrive reminders + reminder data classes + reminders in database displayed in frontend (placeholder)

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Reminder.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+data class ReminderGet(
+    val reminder_title: String?,
+    val reminder_date: String?,
+    val reminder_description: String?,
+    val is_complete: Boolean?,
+    val recurrence_type: String?,
+    val recurrence_day_of_week: Int?,
+    val recurrence_time: String?,
+)
+
+data class ReminderCreate(
+    val reminder_title: String,
+    val reminder_date: String,
+    val reminder_description: String? = null,
+    val is_complete: Boolean,
+    val recurrence_type: String? = null,
+    val recurrence_day_of_week: Int? = null,
+    val recurrence_time: String? = null,
+)
+
+data class ReminderEdit(
+    val reminder_title: String?,
+    val reminder_date: String?,
+    val reminder_description: String?,
+    val is_complete: Boolean?,
+    val recurrence_type: String?,
+    val recurrence_day_of_week: Int?,
+    val recurrence_time: String?,
+)
